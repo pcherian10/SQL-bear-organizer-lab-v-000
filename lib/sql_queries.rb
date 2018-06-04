fe1430d1d8c319a105fcc70c8196b8197db0c280
Paul <pcherian10@gmail.com>
2018-06-04 19:55:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -21,7 +21,7 @@ def select_youngest_bear_and_returns_name_and_age
 end
 
 def selects_most_prominent_color_and_returns_with_count
-  "SELECT color COUNT;"
+  "SELECT COUNT(color) FROM bears;"
   binding.pry
 end
 
